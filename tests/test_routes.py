465488c2dd9260fd864b9de34ebc3bd0849adbae
milzama <milzamzp@gmail.com>
2026-05-12 22:28:49 -0400

    feat: implement read account endpoint and test cases

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -38,6 +38,7 @@ class TestAccountService(TestCase):
     @classmethod
     def tearDownClass(cls):
         """Runs once before test suite"""
+        pass
 
     def setUp(self):
         """Runs before each test"""
@@ -123,4 +124,20 @@ class TestAccountService(TestCase):
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
 
-    # ADD YOUR TEST CASES HERE ...
+    def test_get_account(self):
+        """It should Read a single Account"""
+        account = self._create_accounts(1)[0]
+        resp = self.client.get(
+            f"{BASE_URL}/{account.id}", content_type="application/json"
+        )
+        self.assertEqual(resp.status_code, status.HTTP_200_OK)
+        data = resp.get_json()
+        self.assertEqual(data["name"], account.name)
+
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        # Melakukan GET request ke /accounts/0 (ID 0 diasumsikan tidak pernah ada)
+        resp = self.client.get(f"{BASE_URL}/0")
+        
+        # Memastikan sistem merespons dengan status 404 NOT FOUND
+        self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
